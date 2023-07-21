fbf3460e4222e542ce870d83349a9971db699f84
Ghulam Murtaza <murtazarb99@gmail.com>
2023-07-21 12:06:24 +0500

    fix: commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-import { logo } from "../assets";
+import { logo1 } from "../assets";
 
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
-        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <img src={logo1} alt="logo" className="w-28 object-contain" />
 
         <button
           type="button"
