d07b96cf8d63df0734c717287e504dd6163c3876
Ghulam Murtaza <murtazarb99@gmail.com>
2023-07-21 11:43:13 +0500

    feat: initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() =>
+            window.open(
+              "https://github.com/murtazaghulam99/ai-article-summarizer",
+              "_blank"
+            )
+          }
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="modern_gradient ">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Unleash the Power of Concise Insights!
+        Simplify your reading with our cutting-edge, open-source article
+        summarizer. Say goodbye to lengthy articles and embrace clear,
+        captivating summaries that spark curiosity and boost your
+        understanding."
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
